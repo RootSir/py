0bf73a21c4cf633ff0c9bc42b50086908a4c88dc
root <root@li752-57.members.linode.com>
2015-07-19 15:46:06 +0800

    add ht.py

---
+++ ht.py
@@ -0,0 +1,9 @@
+import urllib2
+
+opener = urllib2.build_opener(
+                urllib2.HTTPHandler(),
+                urllib2.HTTPSHandler(),
+                urllib2.ProxyHandler({'https': 'http://:@117.165.15.215:3128'}))
+                #urllib2.ProxyHandler({'https': 'http://:@218.193.132.35:808'}))
+urllib2.install_opener(opener)
+print urllib2.urlopen('https://iforgot.apple.com/password/verify/appleid',data,10).read()
